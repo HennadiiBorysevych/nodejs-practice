66a854f75975c84231cfb2a1b17479506d1a17f6
Hennadii Borysevych <genuch3@gmail.com>
2023-06-20 14:08:00 +0300

    refactor: changes to controllers

---
+++ controllers/contacts/addContact.js
@@ -0,0 +1,7 @@
+const { Contact } = require("../../models/contact");
+
+const addContact = async (req, res) => {
+  const data = await Contact.create(req.body);
+  res.status(201).json(data);
+};
+module.exports = addContact;
