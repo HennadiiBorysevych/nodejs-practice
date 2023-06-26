b5828ccd0abbf868845b6b562436437d28c95e11
Hennadii Borysevych <genuch3@gmail.com>
2023-06-26 16:43:48 +0300

    feat: avatar upload

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validateBody = require("./validateBody");
 const isValidId = require("./isValid");
 const authenticate = require('./authentification')
+const upload = require('./upload')
 
 module.exports = {
   validateBody,
   isValidId,
-  authenticate
+  authenticate,
+  upload
 };
--- models/user.js
+++ models/user.js
@@ -21,6 +21,7 @@ const userSchema = new Schema({
     type: String,
     default: "",
   },
+  avatarURL: String,
 });
 
 userSchema.post("save", mongooseErrorHandler);
