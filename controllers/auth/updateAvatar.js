8c10adcb1f2d20b0aedeccea90798836723f7616
Hennadii Borysevych <genuch3@gmail.com>
2023-06-27 14:05:33 +0300

    refactor: changes to path

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,16 +1,26 @@
 const path = require("path");
-const fs = require('fs/promises')
+const fs = require("fs/promises");
 const { User } = require("../../models/user");
 
-const avatarsDir = path.join(__dirname, "../", "public", "avatars");
+const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
-     const {_id}=req.user
-  const { path, temporUpload, originalname } = req.file;
-  const resultUpload = path.join(avatarsDir, originalname)
-  await fs.readFile(temporUpload,resultUpload)
-  const avatarURL = path.join('avatars', originalname)
-  await User.findByIdAndUpdate(_id, {avatarURL})
+  const { _id } = req.user;
+  const { path: temporUpload, originalname } = req.file;
+  const resultUpload = path.join(avatarsDir, originalname);
+
+  await fs.rename(temporUpload, resultUpload);
+
+  const avatarURL = path.join(
+    "avatars",
+    originalname
+  );
+
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.status(200).json({
+    message: avatarURL,
+  });
 };
 
 module.exports = updateAvatar;
