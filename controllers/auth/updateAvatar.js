06e405c79f0dd659aa50e9c5ddd86a394bacd57d
Hennadii Borysevych <genuch3@gmail.com>
2023-06-27 14:42:25 +0300

    feat: added jimp lib

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,25 +1,34 @@
 const path = require("path");
 const fs = require("fs/promises");
 const { User } = require("../../models/user");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: temporUpload, originalname } = req.file;
-  const resultUpload = path.join(avatarsDir, originalname);
 
-  await fs.rename(temporUpload, resultUpload);
+  const userAvatarName = `${_id}_${originalname}`;
 
-  const avatarURL = path.join(
-    "avatars",
-    originalname
-  );
+  const resultUpload = path.join(avatarsDir, userAvatarName);
 
-  await User.findByIdAndUpdate(_id, { avatarURL });
+  Jimp.read(temporUpload, (err, avatar) => {
+    if (err) throw err;
+    avatar
+      .resize(250, 250)
+      .quality(60)
+      .write(temporUpload, () => {
+        fs.rename(temporUpload, resultUpload);
 
-  res.status(200).json({
-    message: avatarURL,
+        const avatarURL = path.join("avatars", userAvatarName);
+
+        User.findByIdAndUpdate(_id, { avatarURL }).then(() => {
+          res.status(200).json({
+            message: avatarURL,
+          });
+        });
+      });
   });
 };
 
